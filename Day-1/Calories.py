c0eeb99e4f4ab70246c1551cd225dc4b7598fc61
Kenneth Tran <trank.dev@gmail.com>
2022-12-01 15:05:57 -0500

    Output total calories of top 3 elves

--- Day-1/Calories.py
+++ Day-1/Calories.py
@@ -26,3 +26,14 @@ index_most_calories = [index for index, value in enumerate(
 
 # Output of the elf with the most calories
 print(index_most_calories, most_calories)
+
+# Part 2: Find total number of calories held by
+#         the top 3 elves holding the most calories.
+
+total_calories_top_three = 0
+for i in range(3):
+    most_calories = max(calorie_total_by_elf)
+    total_calories_top_three += most_calories
+    calorie_total_by_elf.remove(most_calories)
+
+print(total_calories_top_three)
