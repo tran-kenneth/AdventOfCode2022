49de63466fffc8431f1b70c238b0341144c3da0c
Kenneth Tran <trank.dev@gmail.com>
2022-12-01 20:37:35 -0500

    Add new score key for part 2

--- Day-2/RockPaperScissors.py
+++ Day-2/RockPaperScissors.py
@@ -29,3 +29,15 @@ print(score_total)
 # X means lose
 # Y means draw
 # Z means win
+
+new_score_key = {
+    'A X': 3+0,
+    'A Y': 1+3,
+    'A Z': 2+6,
+    'B X': 1+0,
+    'B Y': 2+3,
+    'B Z': 3+6,
+    'C X': 2+0,
+    'C Y': 3+3,
+    'C Z': 1+6
+}
