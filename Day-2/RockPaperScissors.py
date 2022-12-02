b8fdf9376cf2c6ddb676111ffd6075a633d98cfa
Kenneth Tran <trank.dev@gmail.com>
2022-12-01 20:32:04 -0500

    Add score key

--- Day-2/RockPaperScissors.py
+++ Day-2/RockPaperScissors.py
@@ -23,3 +23,15 @@ Scores:
 """
 clean_lines = [line[:3] for line in lines]
 print(clean_lines)
+
+score_key = {
+    'A X': 1+3,
+    'A Y': 2+6,
+    'A Z': 3+0,
+    'B X': 1+0,
+    'B Y': 2+3,
+    'B Z': 3+6,
+    'C X': 1+6,
+    'C Y': 2+0,
+    'C Z': 3+3
+}
