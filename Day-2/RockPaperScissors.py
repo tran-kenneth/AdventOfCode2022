1ea4599cba6a6ce0aed92c919b6fa1d3bdd2e315
Kenneth Tran <trank.dev@gmail.com>
2022-12-01 20:31:33 -0500

    Clean up new line character from input

--- Day-2/RockPaperScissors.py
+++ Day-2/RockPaperScissors.py
@@ -2,3 +2,24 @@
 file = open('input2.txt')
 lines = [line for line in file.readlines()]
 file.close()
+
+"""
+A - Rock
+B - Paper
+C - Scissors
+
+X - Rock
+Y - Paper
+Z - Scissors
+
+Scores:
+1 - Choose rock
+2 - Choose paper
+3 - Choose scissors
+
+0 - Lose
+3 - Draw
+6 - Win
+"""
+clean_lines = [line[:3] for line in lines]
+print(clean_lines)
