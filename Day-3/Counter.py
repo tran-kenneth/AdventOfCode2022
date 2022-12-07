aff23664cd561e4c44fd07481657f55ccc1d60dd
Kenneth Tran <trank.dev@gmail.com>
2022-12-07 13:28:12 -0500

    Add dictionary to count score for lowercase letters

--- Day-3/Counter.py
+++ Day-3/Counter.py
@@ -3,4 +3,13 @@ file = open('input3.txt')
 lines = [line for line in file.readlines()]
 file.close()
 
-print(lines)
+# Rucksack letter key values
+rucksack_key_values = {
+    'a': 1, 'b': 2, 'c': 3, 'd': 4,
+    'e': 5, 'f': 6, 'g': 7, 'h': 8,
+    'i': 9, 'j': 10, 'k': 11, 'l': 12,
+    'm': 13, 'n': 14, 'o': 15, 'p': 16,
+    'q': 17, 'r': 18, 's': 19, 't': 20,
+    'u': 21, 'v': 22, 'w': 23, 'x': 24,
+    'y': 25, 'z': 26,
+}
