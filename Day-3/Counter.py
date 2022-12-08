c95b915559c491241ba092bdd9ee7eb40634b28e
Kenneth Tran <trank.dev@gmail.com>
2022-12-08 18:59:29 -0500

    Add capital letter key values

--- Day-3/Counter.py
+++ Day-3/Counter.py
@@ -1,6 +1,6 @@
 # Read input file.
 file = open('input3.txt')
-lines = [line for line in file.readlines()]
+rucksacks = [line for line in file.readlines()]
 file.close()
 
 # Rucksack letter key values
@@ -12,4 +12,14 @@ rucksack_key_values = {
     'q': 17, 'r': 18, 's': 19, 't': 20,
     'u': 21, 'v': 22, 'w': 23, 'x': 24,
     'y': 25, 'z': 26,
+    'A': 1+26, 'B': 2+26, 'C': 3+26, 'D': 4+26,
+    'E': 5+26, 'F': 6+26, 'G': 7+26, 'H': 8+26,
+    'I': 9+26, 'J': 10+26, 'K': 11+26, 'L': 12+26,
+    'M': 13+26, 'N': 14+26, 'O': 15+26, 'P': 16+26,
+    'Q': 17+26, 'R': 18+26, 'S': 19+26, 'T': 20+26,
+    'U': 21+26, 'V': 22+26, 'W': 23+26, 'X': 24+26,
+    'Y': 25+26, 'Z': 26+26,
 }
+
+# Remove new lines from input
+clean_rucksacks = [rucksack[:-1] for rucksack in rucksacks]
